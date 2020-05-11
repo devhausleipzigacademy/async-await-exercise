f74604c570a47c48ef03f7e03a9acb7a7a13d370
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-05-11 16:05:08 +0200

    Promise based solution

--- src/axios-exercise.js
+++ src/axios-exercise.js
@@ -12,23 +12,42 @@ function logAllEvents() {
 }
 
 function logOneEvent(id) {
-  //
+  return axios
+    .get(`http://localhost:3000/events/${id}`)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 function logEventsPaginated(pageIndex, numEventsPerPage) {
-  //
+  return axios
+    .get('http://localhost:3000/events', {
+      params: {
+        _limit: numEventsPerPage,
+        _page: pageIndex,
+      },
+    })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 function createEvent(event) {
-  //
+  return axios.post('http://localhost:3000/events', event);
 }
 
 function updateEvent(id, event) {
-  //
+  return axios.put(`http://localhost:3000/events/${id}`, event);
 }
 
 function deleteEvent(id) {
-  //
+  return axios.delete(`http://localhost:3000/events/${id}`);
 }
 
 module.exports = {
